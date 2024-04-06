0c0f4a42f408d3ebbcd880f1f81d1b03518a5f20
Lerros <108396158+LerrosGr@users.noreply.github.com>
2024-04-06 22:56:37 +0300

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+export function getImages(q) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: '43096972-1afb589163252f275da428b1d',
+    q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+  const url = `${BASE_URL}?${params}`;
+  return fetch(url)
+    .then(res => res.json())
+    .catch(error => {
+      console.error('Error occurred while fetching images:', error);
+      throw error;
+    });
+}
